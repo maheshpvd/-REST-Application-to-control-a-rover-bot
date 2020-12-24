b3321efbef202f206efc758f25a0183b164e8a67
maheshpvd <76062264+maheshpvd@users.noreply.github.com>
2020-12-24 20:12:52 +0530

    Update BotApplication.java

--- bot/src/main/java/com/bot/BotApplication.java
+++ bot/src/main/java/com/bot/BotApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * 
+ * This calls basically launches the Application loading
+ *  all auto/default configurations and scan the base packages
+ *
+ */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class BotApplication {
 
